333efab4a623a2010aa176bb3cb93163bc2b948e
gustavo <valdezgustavo461@gmail.com>
2022-03-25 14:41:55 -0500

    modificado controladores area

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,11 +1,8 @@
 class AreasController < ApplicationController
+  
   def index
     @areas = Area.all
-<<<<<<< HEAD
-    
-=======
     render json: @areas, status: 200
->>>>>>> 852fe336abf1f17da6d20fe042a51e1c5bbcb644
   end
 
   def show
@@ -15,24 +12,14 @@ class AreasController < ApplicationController
  
   def new 
   @area = Area.new
-<<<<<<< HEAD
-  
-=======
   render json: @area, status: 200
->>>>>>> 852fe336abf1f17da6d20fe042a51e1c5bbcb644
   end
   
   
   def create
-<<<<<<< HEAD
     @area = Area.create(
     codigo_area: params[:area][:codigo_area],
     nombre_area: params[:area][:nombre_area])
-=======
-    @area = Area.create!(
-    codigo_area: params[:codigo_area],
-    nombre_area: params[:nombre_area])
->>>>>>> 852fe336abf1f17da6d20fe042a51e1c5bbcb644
     if @area.save 
       redirect_to areas_path 
     else 
@@ -40,17 +27,13 @@ class AreasController < ApplicationController
     end 
     
   end
+  }
   def update 
-<<<<<<< HEAD
     @area =  Area.find(params[:id])
-=======
-    @area =  Area.find(params[:codigo_area])
->>>>>>> 852fe336abf1f17da6d20fe042a51e1c5bbcb644
     Area.update(
     codigo_area: params[:codigo_area],
     nombre_area: params[:nombre_area]
     )
-<<<<<<< HEAD
     
    end 
   
@@ -58,15 +41,9 @@ class AreasController < ApplicationController
     @area =  Area.find(params[:id])
     @area.destroy
     redirect_to areas_path
-=======
+
     render json: @area, status: 200
    end 
   
-  def destroy 
-    @area =  Area.find(params[:codigo_area])
-    Area.destroy!
-    render status: 200
-
->>>>>>> 852fe336abf1f17da6d20fe042a51e1c5bbcb644
-   end
+  
 end
